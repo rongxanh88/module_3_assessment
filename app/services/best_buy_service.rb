5ca31120c8974780b756bf5f8465c0a53422329b
rongxanh88 <rongxanh88@gmail.com>
2017-07-26 12:09:45 -0600

    refactor out call HTTP call into service

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,19 @@
+class BestBuyService
+  def initialize(zip_code)
+    @zip_code = zip_code
+    @connection = Faraday.new(url: "https://api.bestbuy.com/v1")
+
+  end
+
+  def self.get_stores(zip_code)
+    service = BestBuyService.new(zip_code)
+    service.response
+  end
+
+  def response
+    connection.get("/stores(area(#{zip_code},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV['best_buy_api_key']}")
+  end
+
+  private
+    attr_reader :zip_code, :connection
+end
