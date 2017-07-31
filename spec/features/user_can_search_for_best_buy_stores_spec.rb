9cc5d881726365704070c9e41ebf332687304bb8
rongxanh88 <rongxanh88@gmail.com>
2017-07-31 14:09:40 -0600

    add vcr use for spec tests

--- spec/features/user_can_search_for_best_buy_stores_spec.rb
+++ spec/features/user_can_search_for_best_buy_stores_spec.rb
@@ -2,33 +2,35 @@ require "rails_helper"
 
 RSpec.feature "View Best Buy Stores", :type => :feature do
   scenario "user searches for best buy stores within 25 miles" do
-    visit "/"
+    VCR.use_cassette 'features/best_buy' do
+      visit "/"
 
-    fill_in "zip_code", with: "80202"
-    click_on "Search"
+      fill_in "zip_code", with: "80202"
+      click_on "Search"
 
-    expect(page).to have_content('10 Total Stores')
-    #first store
-    expect(page).to have_content('Long Name: Cherry Creek Shopping Center')
-    expect(page).to have_content('City: Denver')
-    expect(page).to have_content('Phone Number: 303-270-9189')
-    expect(page).to have_content('Store Type: Mobile SAS')
+      expect(page).to have_content('10 Total Stores')
+      #first store
+      expect(page).to have_content('Long Name: Cherry Creek Shopping Center')
+      expect(page).to have_content('City: Denver')
+      expect(page).to have_content('Phone Number: 303-270-9189')
+      expect(page).to have_content('Store Type: Mobile SAS')
 
-    #10th store
-    expect(page).to have_content('Long Name: Southglenn')
-    expect(page).to have_content('City: Centennial')
-    expect(page).to have_content('istance: 11.02')
-    expect(page).to have_content('Store Type: Big Box')
+      #10th store
+      expect(page).to have_content('Long Name: Southglenn')
+      expect(page).to have_content('City: Centennial')
+      expect(page).to have_content('istance: 11.02')
+      expect(page).to have_content('Store Type: Big Box')
 
-    within ".paginate" do
-      click_on "2"
+      within ".paginate" do
+        click_on "2"
+      end
+
+      expect(page).to have_content('7 Total Stores')
+      #17th store
+      expect(page).to have_content('Long Name: Boulder')
+      expect(page).to have_content('City: Boulder')
+      expect(page).to have_content('Phone Number: 303-938-2889')
+      expect(page).to have_content('Store Type: Big Box')
     end
-
-    expect(page).to have_content('7 Total Stores')
-    #17th store
-    expect(page).to have_content('Long Name: Boulder')
-    expect(page).to have_content('City: Boulder')
-    expect(page).to have_content('Phone Number: 303-938-2889')
-    expect(page).to have_content('Store Type: Big Box')
   end
 end
--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -2,8 +2,9 @@ require "rails_helper"
 
 RSpec.describe "Sends api call to Best Buy" do
   scenario "search for stores within 25 miles of 80202" do
-    stores = BestBuyService.get_stores("80202")
-    
-    expect(stores.count).to eq(17)
+    VCR.use_cassette 'service/best_buy' do
+      stores = BestBuyService.get_stores("80202")
+      expect(stores.count).to eq(17)
+    end
   end
 end
